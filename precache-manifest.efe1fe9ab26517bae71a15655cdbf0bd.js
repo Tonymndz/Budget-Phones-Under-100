38d05a79ee59d1ee8b124c463393d2885d4eca32
Tonymndz <Tonymndz1141@gmail.com>
2020-09-28 14:35:46 -0700

    Updates

---
+++ precache-manifest.efe1fe9ab26517bae71a15655cdbf0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f94f2837ca9e309109abf521cbed19d",
+    "url": "/BestBudgetPhonesUnder100/index.html"
+  },
+  {
+    "revision": "b2ce19e7cc828a4f4a46",
+    "url": "/BestBudgetPhonesUnder100/static/css/main.b5046783.chunk.css"
+  },
+  {
+    "revision": "c5de005fedbbe36ccc26",
+    "url": "/BestBudgetPhonesUnder100/static/js/2.befa45cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BestBudgetPhonesUnder100/static/js/2.befa45cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ce19e7cc828a4f4a46",
+    "url": "/BestBudgetPhonesUnder100/static/js/main.7973c586.chunk.js"
+  },
+  {
+    "revision": "c7af202757caef7e7dc1",
+    "url": "/BestBudgetPhonesUnder100/static/js/runtime-main.94892846.js"
+  }
+]);
