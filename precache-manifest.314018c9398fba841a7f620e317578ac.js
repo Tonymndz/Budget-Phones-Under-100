38b6480da70b52f9c414550f7d74f0779febbc66
Tonymndz <Tonymndz1141@gmail.com>
2020-09-28 15:06:34 -0700

    Updates

---
+++ precache-manifest.314018c9398fba841a7f620e317578ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b6da681eff28d34c3f12239d142d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6318319dab4fdf6d069",
+    "url": "/static/css/main.b5046783.chunk.css"
+  },
+  {
+    "revision": "c5de005fedbbe36ccc26",
+    "url": "/static/js/2.befa45cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.befa45cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6318319dab4fdf6d069",
+    "url": "/static/js/main.c5c9f3dd.chunk.js"
+  },
+  {
+    "revision": "bd0f0fa040b083c92688",
+    "url": "/static/js/runtime-main.c7a80ba0.js"
+  }
+]);
